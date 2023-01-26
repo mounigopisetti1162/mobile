1ba2d348bc8fd94f72c9cd02ce63635ed8216b8d
mouni <mounika.g1162@gmail.com>
2023-01-26 21:03:22 +0530

    mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
+const API="http://localhost:4001"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+    
+    <div className='App'>
+      helo
+      <Phonelist />
     </div>
-  );
+    
+  )
 }
+
+function Phonelist()
+{
+
+  const [mobile,setmobile]=useState([])
+useEffect(()=>{
+
+  fetch(`${API}/mobile`).then((data)=>data.json()).then((mbs)=>setmobile(mbs))
+},[])
+  return(
+<div className='phone-list-container'>
+  {mobile.map((data,key) =>
+    (
+      <Phone key={key} data={data}/>
+))}
+    </div>
+  )
+}
+
+  function Phone({data})
+    
+    {
+      return(
+    <div className="phone-container">
+      <img className="phone-picture" src={data.img} alt={data.model}/>
+      <h2 className="phone-name">{data.model}</h2>
+      <p className='phone-company'>{data.company}</p>
+
+    </div>
+      )
+}
+  
+    
+
 
 export default App;
